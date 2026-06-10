8f863451a8bf0d3d0af1672c112a654364f63013
Arbind <arbindmalava@gmail.com>
2026-06-10 21:42:03 +0530

    Changes to v1

--- SpeechRecognitionSystem.py
+++ SpeechRecognitionSystem.py
@@ -1,22 +1,28 @@
-import Speech_recognition
+import speech_recognition as sr
 import pyttsx3
+
+Voice = pyttsx3.init()
 
 def Speak(command):
     Voice = pyttsx3.init()
     Voice.say(command)
     Voice.runAndWait()
 
-sr = Speech_recognition.Recognizer()
-with speech_recognition.Microphone() as source2:
+r = sr.Recognizer()
+with sr.Microphone() as source:
     print("Adjusting ambient noise...")
-    sr.adjust_for_ambient_noise(source2, duration = 2)
+    r.adjust_for_ambient_noise(source, duration = 2)
     print("Start Speaking...")
     
-    audio2 = sr.listen(source2)
+    audio2 = r.listen(source, timeout = 3)
     
-    textT = sr.recognize_google(audio2)
-    textT = textT.lower()
-
-    print("Heard text: " + textT)
-
-Speak(textT)
+    
+    try:        
+        textT = r.recognize_google(audio2)
+        textT = textT.lower()
+        print("Heard text: " + textT)
+        Speak(textT)
+    except sr.UnknownValueError:
+        print("unable to recognize speech")
+    except sr.RequestError:
+        print("unable to connect xxxxx")
